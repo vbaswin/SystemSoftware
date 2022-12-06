218cb70f32e99bd494532e52db9b8d6210fd1daf
Aswin V B <aswinvb10a@gmail.com>
2022-12-06 21:18:01 +0530

    me

--- SS/2 pass assembler /pass 1/pass1.c
+++ SS/2 pass assembler /pass 1/pass1.c
@@ -3,74 +3,79 @@
 #include <stdlib.h>
 #include <math.h>
 
-void saveInter(FILE *file, char **code, int len, int LOCCTR) {
-    fprintf(file, "%04X", LOCCTR);
-    for (int i = 0; i < len; ++i) {
-        if (i > 0 && strlen(code[i-1]) < 4)
-            fprintf(file, "\t\t%s", code[i]);
-        else 
-            fprintf(file, "\t%s", code[i]);
+void readSource(FILE *source, char code[10][10]) {
 
-    } 
-}
-void saveSym(FILE *file, char label[], int LOCCTR) {
-    if (strlen(label) < 4)
-        fprintf(file, "%s\t\t%04X\n", label, LOCCTR);
-    else 
-        fprintf(file, "%s\t%04X\n", label, LOCCTR);
-}
+    char str[100];
+    fgets(str, 100, source);
+    int next = 0;
 
-int searchSymtab(FILE *SYMTAB, char key[]) {
-    rewind(SYMTAB);
-    char label[20], address[20];
-    while (fscanf(SYMTAB, "%s %s", label, address) != EOF) {
-        if (!strcmp(key, label)) 
-            return 1;
+    int j = 0, k = 0, i = 0;
+    while (str[i] != 0) {
+        if (str[i] == ' ' || str[i] == '\t') {
+            if (!next) {
+                next = 1;
+                code[j][k] = 0;
+                k = 0;
+                ++j;
+            }
+        }
+        else {
+                next = 0;
+                code[j][k++] = str[i];
+        }
+        ++i;
     }
-    return 0;
+    code[j][k] = 0;
+
+    for (int i = 0; i < j+1; ++i)
+        printf("%s ", code[i]);
 }
 
-int searchOptab(FILE *OPTAB, char key[]) {
-    rewind(OPTAB);
-    char op[20];
-    int opcode;
-    while (fscanf(OPTAB, "%s %06X", op, &opcode) != EOF) {
-        if (!strcmp(op, key))
-            return 1;
-    }
-    return 0;
-}
+// void saveInter(FILE *intermediate, codeL code, int len, int LOCCTR) {
+//     fprintf(intermediate, "%04X %s %s %s", LOCCTR, code.label, code.opcode, code.operand);
 
-char **codeSplit(char str[], int *len){
-    *len = 0;
+// }
+// void saveSym(FILE *file, char label[], int LOCCTR) {
+//     if (strlen(label) < 4)
+//         fprintf(file, "%s\t\t%04X\n", label, LOCCTR);
+//     else 
+//         fprintf(file, "%s\t%04X\n", label, LOCCTR);
+// }
 
-    char ** code = malloc(10 * sizeof(char));
-    for (int i = 0; i < 10; ++i) 
-        code[i] = malloc(10 * sizeof(char));
+// int searchSymtab(FILE *SYMTAB, char key[]) {
+//     rewind(SYMTAB);
+//     char label[20], address[20];
+//     while (fscanf(SYMTAB, "%s %s", label, address) != EOF) {
+//         if (!strcmp(key, label)) 
+//             return 1;
+//     }
+//     return 0;
+// }
 
-    char *hi = strtok(str, " ");
+// int searchOptab(FILE *OPTAB, char key[]) {
+//     rewind(OPTAB);
+//     char op[20];
+//     int opcode;
+//     while (fscanf(OPTAB, "%s %06X", op, &opcode) != EOF) {
+//         if (!strcmp(op, key))
+//             return 1;
+//     }
+//     return 0;
+// }
 
-    while (hi != NULL) {
-        code[(*len)++] = hi;
-        hi = strtok(NULL, " ");
-    }
-
-    return code;
-}
-
-int bytecondition(char str[]) {
-    if (str[0] == 'C') {
-        return strlen(str) - 3;
-    }
-    else if (str[0] == 'X') 
-        return ceil((int)((strlen(str)-2)/2));
-    return 0;
-}
+// int bytecondition(char str[]) {
+//     if (str[0] == 'C') {
+//         return strlen(str) - 3;
+//     }
+//     else if (str[0] == 'X') 
+//         return ceil((int)((strlen(str)-2)/2));
+//     return 0;
+// }
 
 int main() {
     FILE  *source, *OPTAB, *SYMTAB, *intermediate, *infoSave;
     int LOCCTR, startingAddress, current, len;
-    char **code, str[100];
+    char **code;
 
     source = fopen("source.txt", "r");
     OPTAB = fopen("optab.txt", "r");
@@ -82,61 +87,58 @@ int main() {
         printf("Error opening file\n");
         return 0;
     }
-    fgets(str, 100, source);
-    code = codeSplit(str, &len);
+    readSource(source, code);
 
-    if (!strcmp(code[1], "START")) {
-        // convert string to base 16
-        LOCCTR = (int)strtol(code[2], NULL, 16);
-        saveInter(intermediate, code, len, LOCCTR);
-    }
-    else {
-        LOCCTR = 0;
-        rewind(source);
-    }
-    startingAddress = LOCCTR;
+        for (int i = 0; i < 3; ++i)
+        printf("%s ", code[i]);
 
-    fgets(str, 100, source);
-    code = codeSplit(str, &len);
-    while (strcmp(code[1], "END")) {
-        current = LOCCTR;
-        if (strcmp(code[0], "**")) {
-            if (strcmp(code[0], "-")) {
-                if (!searchSymtab(SYMTAB, code[0]))
-                    saveSym(SYMTAB, code[0], LOCCTR); 
-            }
-            if (searchOptab(OPTAB, code[1])) 
-                LOCCTR += 3;
-            else if (!strcmp(code[1], "WORD")) 
-                LOCCTR += 3;
-            else if (!strcmp(code[1], "RESW"))
-                LOCCTR += (3 * atoi(code[2]));
-            else if (!strcmp(code[1], "RESB")) 
-                LOCCTR += atoi(code[2]);
-            else if (!strcmp(code[1], "BYTE")) 
-                LOCCTR += bytecondition(code[2]);
-            else {
-                printf("Error in opcode!\n");
-                fgets(str, 100, source);
-                code = codeSplit(str, &len);
-                continue;
-            }
-        }
-        else {
-            fprintf(intermediate, "%s", code[0]);
-            for (int i = 1; i < len; ++i)
-                fprintf(intermediate, " %s", code[i]);
-            fgets(str, 100, source);
-            code = codeSplit(str, &len);
-            continue;
-        }
-        saveInter(intermediate, code, len, current);
-        fgets(str, 100, source);
-        code = codeSplit(str, &len);
-    }
-    saveInter(intermediate, code, len, LOCCTR);
+    // printf("today: %s %s %s\n", d.code[0], d.code[1], d.code[2]);
 
-    fprintf(infoSave, "ProgramLength: %4X", (LOCCTR-startingAddress));
+    // if (!strcmp(code.opcode, "START")) {
+    //     // convert string to base 16
+    //     LOCCTR = (int)strtol(code.operand, NULL, 16);
+    //     saveInter(intermediate, code, len, LOCCTR);
+    // }
+    // else {
+    //     LOCCTR = 0;
+    //     rewind(source);
+    // }
+    // startingAddress = LOCCTR;
+
+    // readSource(source, &code);
+    // printf("%s %s %s", code.label, code.opcode, code.operand);
+    // while (strcmp(code.opcode, "END")) {
+    //     // current = LOCCTR;
+    //     printf("%s %s %s", code.label, code.opcode, code.operand);
+    //     if (strcmp(code.label, "**")) {
+    //         if (strcmp(code.label, "-")) {
+    //             if (!searchSymtab(SYMTAB, code.label))
+    //                 saveSym(SYMTAB, code.label, LOCCTR); 
+    //         }
+    //         if (searchOptab(OPTAB, code.opcode)) 
+    //             LOCCTR += 3;
+    //         else if (!strcmp(code.opcode, "WORD")) 
+    //             LOCCTR += 3;
+    //         else if (!strcmp(code.opcode, "RESW"))
+    //             LOCCTR += (3 * atoi(code.operand));
+    //         else if (!strcmp(code.opcode, "RESB")) 
+    //             LOCCTR += atoi(code.operand);
+    //         else if (!strcmp(code.opcode, "BYTE")) 
+    //             LOCCTR += bytecondition(code.operand);
+    //         else {
+    //             printf("Error in opcode!\n");
+    //             readSource(source, &code);
+    //             continue;
+    //         }
+    //     }
+    //     else { 
+    //         readSource(source, &code);
+    //         continue;
+    //     }
+    //     saveInter(intermediate, code, len, current);
+    //     readSource(source, &code);
+    // }
+    // fprintf(infoSave, "ProgramLength: %4X", (LOCCTR-startingAddress));
 
     fclose(source);
     fclose(OPTAB);
@@ -146,3 +148,7 @@ int main() {
     return 0;
 }
 
+// owlcity - fireflies
+// zootopia - last song
+// counting stars
+// 7 years
