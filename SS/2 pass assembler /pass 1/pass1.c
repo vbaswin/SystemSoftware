b8365e12c01763bedbfe49ad6047c771222f0a01
vbaswin <aswinvb10a@gmail.com>
2022-12-11 15:59:56 +0530

    finally did some parsing, instead of thinking complex worked on making it as simple as possible

--- SS/2 pass assembler /pass 1/pass1.c
+++ SS/2 pass assembler /pass 1/pass1.c
@@ -1,81 +1,81 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
-void readSource(FILE *source, char code[10][10]) {
-
-    char str[100];
-    fgets(str, 100, source);
-    int next = 0;
-
-    int j = 0, k = 0, i = 0;
-    while (str[i] != 0) {
-        if (str[i] == ' ' || str[i] == '\t') {
-            if (!next) {
-                next = 1;
+int parseSpTb (char code[][20], FILE *source) {
+    int j = 0, k = 0, sz = 0;
+    char ch;
+    while ((ch = fgetc(source)) != '\n' && ch != EOF) { 
+        if(ch == ' ' || ch == '\t'){
+            if (sz) {
+                sz = 0;
                 code[j][k] = 0;
                 k = 0;
                 ++j;
-            }
+            } else 
+                continue;
+        } else {
+            sz = 1;
+            code[j][k++] = ch;
         }
-        else {
-                next = 0;
-                code[j][k++] = str[i];
-        }
-        ++i;
     }
+    if (!sz)
+        return j;
     code[j][k] = 0;
-
-    for (int i = 0; i < j+1; ++i)
-        printf("%s ", code[i]);
+    return j+1;
 }
 
-// void saveInter(FILE *intermediate, codeL code, int len, int LOCCTR) {
-//     fprintf(intermediate, "%04X %s %s %s", LOCCTR, code.label, code.opcode, code.operand);
+void saveInter(FILE *intermediate, char code[][20], int len, int LOCCTR) {
+    fprintf(intermediate, "%04X\t%s", LOCCTR, code[0]);
+    for (int i = 1; i < 3; ++i) {
+        if (strlen(code[i-1]) < 4) 
+            fprintf(intermediate,"\t");
+        fprintf(intermediate,"\t%s", code[i]);
+    }
+    fprintf(intermediate,"\n");
+}
+void saveSym(FILE *file, char label[], int LOCCTR) {
+    if (strlen(label) < 4)
+        fprintf(file, "%s\t\t%04X\n", label, LOCCTR);
+    else 
+        fprintf(file, "%s\t%04X\n", label, LOCCTR);
+}
 
-// }
-// void saveSym(FILE *file, char label[], int LOCCTR) {
-//     if (strlen(label) < 4)
-//         fprintf(file, "%s\t\t%04X\n", label, LOCCTR);
-//     else 
-//         fprintf(file, "%s\t%04X\n", label, LOCCTR);
-// }
+int searchSymtab(FILE *SYMTAB, char key[]) {
+    rewind(SYMTAB);
+    char label[20], address[20];
+    while (fscanf(SYMTAB, "%s %s", label, address) != EOF) {
+        if (!strcmp(key, label)) 
+            return 1;
+    }
+    return 0;
+}
 
-// int searchSymtab(FILE *SYMTAB, char key[]) {
-//     rewind(SYMTAB);
-//     char label[20], address[20];
-//     while (fscanf(SYMTAB, "%s %s", label, address) != EOF) {
-//         if (!strcmp(key, label)) 
-//             return 1;
-//     }
-//     return 0;
-// }
+int searchOptab(FILE *OPTAB, char key[]) {
+    rewind(OPTAB);
+    char op[20];
+    int opcode;
+    while (fscanf(OPTAB, "%s %06X", op, &opcode) != EOF) {
+        if (!strcmp(op, key))
+            return 1;
+    }
+    return 0;
+}
 
-// int searchOptab(FILE *OPTAB, char key[]) {
-//     rewind(OPTAB);
-//     char op[20];
-//     int opcode;
-//     while (fscanf(OPTAB, "%s %06X", op, &opcode) != EOF) {
-//         if (!strcmp(op, key))
-//             return 1;
-//     }
-//     return 0;
-// }
-
-// int bytecondition(char str[]) {
-//     if (str[0] == 'C') {
-//         return strlen(str) - 3;
-//     }
-//     else if (str[0] == 'X') 
-//         return ceil((int)((strlen(str)-2)/2));
-//     return 0;
-// }
+int bytecondition(char str[]) {
+    if (str[0] == 'C') {
+        return strlen(str) - 3;
+    }
+    else if (str[0] == 'X') 
+        return ceil((int)((strlen(str)-2)/2));
+    return 0;
+}
 
 int main() {
     FILE  *source, *OPTAB, *SYMTAB, *intermediate, *infoSave;
     int LOCCTR, startingAddress, current, len;
-    char **code;
+    char code[100][20];
 
     source = fopen("source.txt", "r");
     OPTAB = fopen("optab.txt", "r");
@@ -87,58 +87,52 @@ int main() {
         printf("Error opening file\n");
         return 0;
     }
-    readSource(source, code);
 
-        for (int i = 0; i < 3; ++i)
-        printf("%s ", code[i]);
+    len = parseSpTb(code, source);
+    if (!strcmp(code[1], "START")) {
+        // convert string to base 16
+        LOCCTR = (int)strtol(code[2], NULL, 16);
+        saveInter(intermediate, code, len, LOCCTR);
+    }
+    else {
+        LOCCTR = 0;
+        rewind(source);
+    }
+    startingAddress = LOCCTR;
 
-    // printf("today: %s %s %s\n", d.code[0], d.code[1], d.code[2]);
-
-    // if (!strcmp(code.opcode, "START")) {
-    //     // convert string to base 16
-    //     LOCCTR = (int)strtol(code.operand, NULL, 16);
-    //     saveInter(intermediate, code, len, LOCCTR);
-    // }
-    // else {
-    //     LOCCTR = 0;
-    //     rewind(source);
-    // }
-    // startingAddress = LOCCTR;
-
-    // readSource(source, &code);
-    // printf("%s %s %s", code.label, code.opcode, code.operand);
-    // while (strcmp(code.opcode, "END")) {
-    //     // current = LOCCTR;
-    //     printf("%s %s %s", code.label, code.opcode, code.operand);
-    //     if (strcmp(code.label, "**")) {
-    //         if (strcmp(code.label, "-")) {
-    //             if (!searchSymtab(SYMTAB, code.label))
-    //                 saveSym(SYMTAB, code.label, LOCCTR); 
-    //         }
-    //         if (searchOptab(OPTAB, code.opcode)) 
-    //             LOCCTR += 3;
-    //         else if (!strcmp(code.opcode, "WORD")) 
-    //             LOCCTR += 3;
-    //         else if (!strcmp(code.opcode, "RESW"))
-    //             LOCCTR += (3 * atoi(code.operand));
-    //         else if (!strcmp(code.opcode, "RESB")) 
-    //             LOCCTR += atoi(code.operand);
-    //         else if (!strcmp(code.opcode, "BYTE")) 
-    //             LOCCTR += bytecondition(code.operand);
-    //         else {
-    //             printf("Error in opcode!\n");
-    //             readSource(source, &code);
-    //             continue;
-    //         }
-    //     }
-    //     else { 
-    //         readSource(source, &code);
-    //         continue;
-    //     }
-    //     saveInter(intermediate, code, len, current);
-    //     readSource(source, &code);
-    // }
-    // fprintf(infoSave, "ProgramLength: %4X", (LOCCTR-startingAddress));
+    len = parseSpTb(code, source);
+    while (strcmp(code[1], "END")) {
+        current = LOCCTR;
+        printf("%s %s %s\n", code[0], code[1], code[2]);
+        if (strcmp(code[0], "**")) {
+            if (strcmp(code[0], "-")) {
+                if (!searchSymtab(SYMTAB, code[0]))
+                    saveSym(SYMTAB, code[0], LOCCTR); 
+            }
+            if (searchOptab(OPTAB, code[1])) 
+                LOCCTR += 3;
+            else if (!strcmp(code[1], "WORD")) 
+                LOCCTR += 3;
+            else if (!strcmp(code[1], "RESW"))
+                LOCCTR += (3 * atoi(code[2]));
+            else if (!strcmp(code[1], "RESB")) 
+                LOCCTR += atoi(code[2]);
+            else if (!strcmp(code[1], "BYTE")) 
+                LOCCTR += bytecondition(code[2]);
+            else {
+                printf("Error in opcode!\n");
+                len = parseSpTb(code, source);
+                continue;
+            }
+        }
+        else { 
+            len = parseSpTb(code, source);
+            continue;
+        }
+        saveInter(intermediate, code, len, current);
+        len = parseSpTb(code, source);
+    }
+    fprintf(infoSave, "ProgramLength: %4X", (LOCCTR-startingAddress));
 
     fclose(source);
     fclose(OPTAB);
@@ -147,8 +141,3 @@ int main() {
     fclose(infoSave);
     return 0;
 }
-
-// owlcity - fireflies
-// zootopia - last song
-// counting stars
-// 7 years
--- SS/practise/1.c
+++ SS/practise/1.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+int parse (char code[][10], FILE *source) {
+    int i = 0, j = 0, k = 0;
+    int next_token = 0;
+    char ch;
+    while ((ch = fgetc(source)) != '\n') {
+        if(ch == ' ' ||ch == '\t'){
+            if(!next_token){
+                next_token = 1;
+                code[j][k] = 0;
+                k = 0;
+                j++;
+            }
+        }
+        else{
+            next_token = 0;
+            code[j][k++] = ch;
+        }
+        i++;
+    }
+    code[j][k] = 0;
+    return j+1;
+}
 
 int main() {
-    FILE *fptr;
-    fptr = fopen("aswin.txt", "r+");
-    char str[50];
-    while (fgets(str, 50, fptr) != NULL) 
-        printf("%s", str);
-    fseek(fptr, 0, SEEK_SET);
-    while (fgets(str, 50, fptr) != NULL) 
-        printf("%s", str);
-    fclose(fptr);
+    FILE *source = fopen("aswin.txt", "rt");
+    char code[100][10];
+    int len;
+    len = parse(code, source);
+    printf("\ninside\n");
+    for (int i = 0; i < len; ++i)
+        printf("%s ", code[i]);
+
     return 0;
 }
